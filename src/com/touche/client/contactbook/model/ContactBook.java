ba44df2d7e333dfd945a69af318c20a6d344a090
Roby <roberta.btt@gmail.com>
2017-11-12 12:16:52 +0100

    Added the link to the method readSource. This method is call 'performReadSource' and inside it calls the generic readSource. The  implementation of the method readSource depends on the specific class

--- src/com/touche/client/contactbook/model/ContactBook.java
+++ src/com/touche/client/contactbook/model/ContactBook.java
@@ -4,6 +4,8 @@
  */
 package com.touche.client.contactbook.model;
 
+import java.util.List;
+
 import com.touche.client.contactbook.messagesender.*;
 import com.touche.client.contactbook.sourcereader.*;
 
@@ -26,6 +28,12 @@ public abstract class ContactBook{
 		sendMessageBehaviour = smb;
 	}
 	
+	
+	/*Perform the action. The concrete class that implements
+	 * those behaviors, will do the action.*/
+	public List<String> performReadSource(String source) {
+		return sourceReaderBehaviour.readSource(source);
+	}
 	
 	/*Check what classes have been setted*/
 	public SourceReaderBehaviour getSourceReaderBehaviour(){
