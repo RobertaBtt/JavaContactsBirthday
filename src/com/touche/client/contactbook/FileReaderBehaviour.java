044e3c4d4ba08314e130b6fdf2c9a17ad3c051ed
Roby <roberta.btt@gmail.com>
2017-11-12 10:31:52 +0100

    First two tests to get lines from a test file and from an empty test file. Class FileReaderBehaviour added

---
+++ src/com/touche/client/contactbook/FileReaderBehaviour.java
@@ -0,0 +1,49 @@
+package com.touche.client.contactbook;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class FileReaderBehaviour {
+
+	public List<String> readSource(String source) {
+		
+	
+		List<String> linesFromSource = null;
+		
+		try {
+				linesFromSource = readContactsFromLines(source);
+			
+		} catch (FileNotFoundException e) {
+			System.out.println ("File not Found" + source);	
+			e.printStackTrace();
+		}
+				
+		return linesFromSource;
+	}
+	
+	
+	private List<String> readContactsFromLines(String source) throws FileNotFoundException{
+		
+		List<String> linesFromSource = new ArrayList<String>();
+		Scanner fileScanner;
+		try{
+			fileScanner = new Scanner(new File(source));
+			while (fileScanner.hasNextLine()){
+				linesFromSource.add(fileScanner.nextLine());				
+			}
+			fileScanner.close();
+		}
+		catch (FileNotFoundException e) {
+			linesFromSource = new ArrayList<String>();
+			throw e;
+		}
+		
+		return linesFromSource;
+		
+	}
+	
+}
---
+++ src/test/FileReaderBehaviourTest.java
@@ -0,0 +1,41 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.touche.client.contactbook.FileReaderBehaviour;
+
+
+public class FileReaderBehaviourTest {
+	
+	private  static final String FILE_EMPTY = "EmptyTestInput.txt";
+	private  static final String FILE = "TestInput.txt";
+	private  static final int NUMBER_OF_LINES = 10;
+	private FileReaderBehaviour fileReaderBehaviour;
+	
+	@Before
+	public void setUp() throws Exception {		
+		fileReaderBehaviour = new FileReaderBehaviour();
+	}
+	
+	@Test
+	public final void testGetLinesFromSource(){		
+		List<String> linesFromSource = fileReaderBehaviour.readSource(FILE);
+		assertEquals(NUMBER_OF_LINES, linesFromSource.size());
+	}
+	
+	@Test
+	public final void testGetLinesFromSourceEmpty(){		
+		List<String> linesFromSource = fileReaderBehaviour.readSource(FILE_EMPTY);
+		assertEquals(0, linesFromSource.size());
+	}
+	
+	@Test
+	public final void testGetFirstLine(){		
+		List<String> linesFromSource = fileReaderBehaviour.readSource(FILE);
+		assertEquals("Doe, John, 1982/10/08, john.doe@foobar.com", linesFromSource.get(1));
+	}
+}
